b098f6879442b467f0bd4db3c26cdcea3db3759a
mariampervez <mariam.pervez@nav.no>
2021-03-23 10:47:52 +0100

    Forsøker å legge til @EnableRetry
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/EessiPensjonKravInitialiseringApplication.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/EessiPensjonKravInitialiseringApplication.kt
@@ -2,7 +2,9 @@ package no.nav.eessi.pensjon
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.retry.annotation.EnableRetry
 
+@EnableRetry
 @SpringBootApplication
 class EessiPensjonKravInitialiseringApplication
 
