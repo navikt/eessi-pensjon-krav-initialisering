8e0eaa4a8088dd90b8b7d7b42b45972e6d835c5a
Daniel Skarpås <dskarpas@gmail.com>
2021-04-08 10:30:30 +0200

    S3 creds for q1, q2

--- nais/export-vault-secrets.sh
+++ nais/export-vault-secrets.sh
@@ -13,3 +13,10 @@ then
     echo "Setter eessi-pensjon-krav-initialisering srvUsername"
     export srvusername=$(cat /var/run/secrets/nais.io/srveessipensjonkrav/username)
 fi
+
+echo "Sjekker eessi_pensjon_krav_s3_creds_password"
+if test -f /var/run/secrets/nais.io/appcredentials/eessi_pensjon_krav_s3_creds_password;
+then
+  echo "Setter eessi_pensjon_krav_s3_creds_password"
+    export eessi_pensjon_krav_s3_creds_password=$(cat /var/run/secrets/nais.io/appcredentials/eessi_pensjon_krav_s3_creds_password)
+fi
