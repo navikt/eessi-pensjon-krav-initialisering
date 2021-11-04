0cf25039c80390c9f45aefe1a0bcbd2af9174de5
Mo Amini <mo.amini@nav.no>
2021-11-04 14:00:37 +0100

    Går over til å lese topics fra aiven (#6)
    Mo gjorde dette

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
@@ -2,10 +2,8 @@ package no.nav.eessi.pensjon.config
 
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.common.config.SaslConfigs
 import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,9 +14,6 @@ import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.listener.ContainerProperties
 import org.springframework.kafka.support.serializer.JsonDeserializer
-import org.springframework.retry.backoff.FixedBackOffPolicy
-import org.springframework.retry.policy.SimpleRetryPolicy
-import org.springframework.retry.support.RetryTemplate
 import java.time.Duration
 
 @EnableKafka
@@ -29,11 +24,7 @@ class KafkaConfig(
     @param:Value("\${kafka.credstore.password}") private val credstorePassword: String,
     @param:Value("\${kafka.truststore.path}") private val truststorePath: String,
     @param:Value("\${kafka.brokers}") private val aivenBootstrapServers: String,
-    @param:Value("\${ONPREM_KAFKA_BOOTSTRAP_SERVERS_URL}") private val onpremBootstrapServers: String,
     @param:Value("\${kafka.security.protocol}") private val securityProtocol: String,
-    @param:Value("\${srvusername}") private val srvusername: String,
-    @param:Value("\${srvpassword}") private val srvpassword: String,
-    @Autowired val kafkaErrorHandler: KafkaCustomErrorHandler
 ) {
 
     fun aivenKafkaConsumerFactory(): ConsumerFactory<String, String> {
@@ -50,36 +41,12 @@ class KafkaConfig(
         return DefaultKafkaConsumerFactory(configMap, keyDeserializer, valueDeserializer)
     }
 
-    fun onpremKafkaConsumerFactory(): ConsumerFactory<String, String> {
-        val keyDeserializer: JsonDeserializer<String> = JsonDeserializer(String::class.java)
-        keyDeserializer.setUseTypeHeaders(false)
-        val configMap: MutableMap<String, Any> = HashMap()
-        populerOnpremCommonConfig(configMap)
-        configMap[ConsumerConfig.CLIENT_ID_CONFIG] = "eessi-pensjon-krav-initialisering"
-        configMap[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = onpremBootstrapServers
-        configMap[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
-        configMap[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
-        configMap[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = 1
-        return DefaultKafkaConsumerFactory(configMap, StringDeserializer(), JsonDeserializer())
-    }
-
     @Bean
     fun aivenKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
         val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
         factory.consumerFactory = aivenKafkaConsumerFactory()
         factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL
         factory.containerProperties.authorizationExceptionRetryInterval =  Duration.ofSeconds(4L)
-        return factory
-    }
-
-    @Bean
-    fun onpremKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
-        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
-        factory.consumerFactory = onpremKafkaConsumerFactory()
-        factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL
-        factory.containerProperties.authorizationExceptionRetryInterval =  Duration.ofSeconds(4L)
-        factory.setErrorHandler(kafkaErrorHandler)
-        factory.setRetryTemplate(retryTemplate())
         return factory
     }
 
@@ -93,79 +60,4 @@ class KafkaConfig(
         configMap[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = truststorePath
         configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = securityProtocol
     }
-
-    private fun populerOnpremCommonConfig(configMap: MutableMap<String, Any>) {
-        configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = "SASL_SSL"
-        configMap[SaslConfigs.SASL_MECHANISM] = "PLAIN"
-        configMap[SaslConfigs.SASL_JAAS_CONFIG] = "org.apache.kafka.common.security.plain.PlainLoginModule required username=${srvusername} password=${srvpassword};"
-    }
-
-    fun retryTemplate(): RetryTemplate {
-        val retryTemplate = RetryTemplate()
-
-        val fixedBackOffPolicy = FixedBackOffPolicy()
-        fixedBackOffPolicy.backOffPeriod = 1000
-        retryTemplate.setBackOffPolicy(fixedBackOffPolicy)
-
-        val retryPolicy = SimpleRetryPolicy()
-        retryPolicy.maxAttempts = 1
-        retryTemplate.setRetryPolicy(retryPolicy)
-
-        return retryTemplate
-    }
-
 }
-
-//import org.apache.kafka.clients.consumer.Consumer
-//import org.apache.kafka.clients.consumer.ConsumerRecord
-//import org.slf4j.LoggerFactory
-//import org.springframework.boot.ApplicationRunner
-//import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.context.annotation.Profile
-//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-//import org.springframework.kafka.config.KafkaListenerEndpointRegistry
-//import org.springframework.kafka.core.ConsumerFactory
-//import org.springframework.kafka.listener.ContainerAwareErrorHandler
-//import org.springframework.kafka.listener.ContainerStoppingErrorHandler
-//import org.springframework.kafka.listener.MessageListenerContainer
-//import org.springframework.kafka.listener.SeekToCurrentErrorHandler
-//import org.springframework.retry.backoff.FixedBackOffPolicy
-//import org.springframework.retry.policy.SimpleRetryPolicy
-//import org.springframework.retry.support.RetryTemplate
-//import org.springframework.util.backoff.FixedBackOff
-//import java.io.PrintWriter
-//import java.io.StringWriter
-//import java.time.Duration
-//
-//@Configuration
-//class KafkaConfig {
-//
-//    @Bean
-//    fun sedSendtAuthRetry(registry: KafkaListenerEndpointRegistry): ApplicationRunner? {
-//        return ApplicationRunner {
-//            val kravInitialiseringListener = registry.getListenerContainer("kravInitialiseringListener")
-//            kravInitialiseringListener.containerProperties.authorizationExceptionRetryInterval = Duration.ofSeconds(4L)
-//            kravInitialiseringListener.start()
-//          }
-//    }
-//
-//    @Bean
-//    fun kafkaListenerContainerFactory(configurer: ConcurrentKafkaListenerContainerFactoryConfigurer,
-//                                      kafkaConsumerFactory: ConsumerFactory<Any, Any>): ConcurrentKafkaListenerContainerFactory<*, *>  {
-//        val factory = ConcurrentKafkaListenerContainerFactory<Any, Any>()
-//        factory.setRetryTemplate(retryTemplate())
-//        configurer.configure(factory, kafkaConsumerFactory)
-//        //factory.setErrorHandler(KafkaCustomErrorHandlerBean())
-//        factory.setErrorHandler(SeekToCurrentErrorHandler(FixedBackOff(1,0)))
-//        return factory
-//    }
-//
-
-//
-//    @Profile("prod")
-//    @Bean
-//    fun kafkaCustomErrorHandlerBean() : KafkaCustomErrorHandler{
-//        return KafkaCustomErrorHandler()
-//    }
--- src/test/kotlin/no/nav/eessi/pensjon/kravinitialisering/config/IntegrasjonsTestConfig.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/kravinitialisering/config/IntegrasjonsTestConfig.kt
@@ -3,6 +3,7 @@ import no.nav.eessi.pensjon.config.KafkaCustomErrorHandler
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,6 +18,7 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.listener.ContainerProperties
+import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.test.EmbeddedKafkaBroker
 import java.time.Duration
 
@@ -25,57 +27,30 @@ class IntegrasjonsTestConfig(@Autowired val kafkaErrorHandler: KafkaCustomErrorH
     @Value("\${" + EmbeddedKafkaBroker.SPRING_EMBEDDED_KAFKA_BROKERS + "}")
     private lateinit var brokerAddresses: String
 
-    @Bean
-    fun producerFactory(): ProducerFactory<String, String> {
-        val configs = HashMap<String, Any>()
-        configs[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = this.brokerAddresses
-        configs[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        configs[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        return DefaultKafkaProducerFactory(configs)
+    fun aivenKafkaConsumerFactory(): ConsumerFactory<String, String> {
+        val keyDeserializer: JsonDeserializer<String> = JsonDeserializer(String::class.java)
+        keyDeserializer.setRemoveTypeHeaders(true)
+        keyDeserializer.addTrustedPackages("*")
+        keyDeserializer.setUseTypeHeaders(false)
+        val valueDeserializer = StringDeserializer()
+        val configMap: MutableMap<String, Any> = HashMap()
+        populerAivenCommonConfig(configMap)
+        configMap[ConsumerConfig.CLIENT_ID_CONFIG] = "eessi-pensjon-krav-initialisering"
+        configMap[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = brokerAddresses
+        configMap[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
+        return DefaultKafkaConsumerFactory(configMap, keyDeserializer, valueDeserializer)
     }
 
     @Bean
-    fun consumerFactory(): ConsumerFactory<String, String> {
-        val configs = HashMap<String, Any>()
-        configs[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = this.brokerAddresses
-        configs[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        configs[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        configs[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
-        configs[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
-        configs[ConsumerConfig.GROUP_ID_CONFIG] = "eessi-pensjon-group-test"
-
-        return DefaultKafkaConsumerFactory(configs)
-    }
-
-    @Bean
-    fun onpremKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
+    fun aivenKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
         val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
-        factory.consumerFactory = kafkaConsumerFactory()
+        factory.consumerFactory = aivenKafkaConsumerFactory()
         factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL
         factory.containerProperties.authorizationExceptionRetryInterval =  Duration.ofSeconds(4L)
-        factory.setErrorHandler(kafkaErrorHandler)
         return factory
     }
 
-    fun kafkaConsumerFactory(): ConsumerFactory<String, String> {
-        val configMap: MutableMap<String, Any> = HashMap()
-        populerCommonConfig(configMap)
-        configMap[ConsumerConfig.CLIENT_ID_CONFIG] = "eessi-pensjon-krav-initialisering"
-        configMap[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        configMap[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        configMap[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = brokerAddresses
-        configMap[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
-
-        return DefaultKafkaConsumerFactory(configMap)
-    }
-
-    private fun populerCommonConfig(configMap: MutableMap<String, Any>) {
+    private fun populerAivenCommonConfig(configMap: MutableMap<String, Any>) {
         configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = "PLAINTEXT"
-    }
-
-    @Bean
-    @Primary
-    fun kafkaTemplate(): KafkaTemplate<String, String> {
-        return KafkaTemplate(producerFactory())
     }
 }
