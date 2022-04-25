fdd0a2712fb794409efe668a01d2fd0f9a377ac7
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-04-25 14:38:55 +0200

    Bytte ut deprekert ErrorHandler

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaCustomErrorHandler.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaCustomErrorHandler.kt
@@ -3,8 +3,8 @@ package no.nav.eessi.pensjon.config
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
+import org.springframework.kafka.listener.CommonContainerStoppingErrorHandler
 import org.springframework.kafka.listener.ContainerAwareErrorHandler
-import org.springframework.kafka.listener.ContainerStoppingErrorHandler
 import org.springframework.kafka.listener.MessageListenerContainer
 import org.springframework.stereotype.Component
 import java.io.PrintWriter
@@ -14,7 +14,7 @@ import java.io.StringWriter
 class KafkaCustomErrorHandler : ContainerAwareErrorHandler {
     private val logger = LoggerFactory.getLogger(KafkaCustomErrorHandler::class.java)
 
-    private val stopper = ContainerStoppingErrorHandler()
+    private val stopper = CommonContainerStoppingErrorHandler()
 
     override fun handle(thrownException: java.lang.Exception,
                         records: MutableList<ConsumerRecord<*, *>>?,
@@ -24,11 +24,9 @@ class KafkaCustomErrorHandler : ContainerAwareErrorHandler {
 
         thrownException.printStackTrace(PrintWriter(stacktrace))
 
-        logger.error(
-            "En feil oppstod under kafka konsumering av meldinger: \n ${hentMeldinger(records)} \n" +
-                    "Stopper containeren ! Restart er nødvendig for å fortsette konsumering, $stacktrace"
-        )
-        stopper.handle(thrownException, records, consumer, container)
+        logger.error("En feil oppstod under kafka konsumering av meldinger: \n ${hentMeldinger(records)} \n" +
+                "Stopper containeren ! Restart er nødvendig for å fortsette konsumering, $stacktrace")
+        stopper.handleRemaining(thrownException, records?: emptyList(), consumer, container)
     }
 
     fun hentMeldinger(records: MutableList<ConsumerRecord<*, *>>?): String {
