f3bf03d90ffccbd9a4c2cc7ffb89965154f89344
Daniel Skarpås <dskarpas@gmail.com>
2024-12-12 14:37:40 +0100

    B - Legger inn logger på logging av info

--- src/main/kotlin/no/nav/eessi/pensjon/kravinitialisering/behandlehendelse/BehandleHendelseKlient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/kravinitialisering/behandlehendelse/BehandleHendelseKlient.kt
@@ -65,7 +65,7 @@ class BehandleHendelseKlient(
                     "/",
                     httpEntity,
                     String::class.java
-                ).also { """********** Kvittering fra opprettelse av krav mot PEN ************ | PostResponse: ${it.body}""" }
+                ).also { logger.info("""********** Kvittering fra opprettelse av krav mot PEN ************ | PostResponse: ${it.body}""") }
             } catch (ex: HttpStatusCodeException) {
                 logger.error("En feil oppstod under opprettelse av behandlehendlse ex: ", ex)
                 throw RuntimeException("En feil oppstod under opprettelse av behandlehendelse ex: ${ex.message} body: ${ex.responseBodyAsString}")
